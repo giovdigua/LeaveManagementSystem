bb6fa8d38db0d8b000213ef83cf0a40bef460747
Giovanni <giovdigua@gmail.com>
2024-09-25 19:42:56 +0200

    Finsih lecture 79

--- LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeReadOnlyVM.cs
+++ LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeReadOnlyVM.cs
@@ -2,10 +2,10 @@
 
 namespace LeaveManagementSystem.Web.Models.LeaveTypes
 {
-    public class IndexVM
+    public class LeaveTypeReadOnlyVM
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public int Days { get; set; }
+        public int NumberOfDays { get; set; }
     }
 }
