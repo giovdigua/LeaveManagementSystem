3b9276300033580e9030513f0c65b0451ab88f5b
Giovanni <giovdigua@gmail.com>
2024-10-06 10:20:34 +0200

    Finsish section 11 User Authentication and Authorization a and confirm mail settings

--- LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -5,6 +5,7 @@ using LeaveManagementSystem.Web.Services;
 
 namespace LeaveManagementSystem.Web.Controllers
 {
+    [Authorize(Roles = Roles.Administrator)]
     public class LeaveTypesController(ILeaveTypeService _leaveTypeService) : Controller
     {
       
